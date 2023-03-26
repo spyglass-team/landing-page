846817600c3b7de66aff8497e052f3caa70726f7
Cindy Chau <cindyxchau1@gmail.com>
2023-03-25 20:13:16 -0400

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { NextUIProvider, createTheme } from '@nextui-org/react';
+const darkTheme = createTheme({ type: 'dark' });
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
+
+export default MyApp;
