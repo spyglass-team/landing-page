68d2dcc899bac25f43ef1c0270a434b7fce06357
Cindy Chau <cindyxchau1@gmail.com>
2023-03-27 18:32:13 -0400

    modified key features on home page

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -5,24 +5,49 @@ import styles from '../styles/Features.module.css';
 const Features = () => {
   return (
     <div className={styles.container}>
-      <Image
-        src='/demo-gifs/local-cluster-metrics.gif'
-        alt='local-cluster-metrics-gif'
-        width='800'
-        height='400'
-      />
-      <Image
-        src='/demo-gifs/cost-analysis.gif'
-        alt='cost-analysis-gif'
-        width='800'
-        height='400'
-      />
-      <Image
-        src='/demo-gifs/visualizer.gif'
-        alt='cluster-visualizer-gif'
-        width='800'
-        height='400'
-      />
+      {/* local cluster metrics */}
+      <div className={styles.imageContainer}>
+        <h1 className={styles.h1}>
+          Monitor Kubernetes cluster health metrics in real-time using{' '}
+          <span>Grafana.</span>
+        </h1>
+        <h2 className={styles.h2}></h2>
+        <Image
+          className={styles.gif}
+          src='/demo-gifs/local-cluster-metrics.gif'
+          alt='local-cluster-metrics-gif'
+          width='850'
+          height='400'
+        />
+      </div>
+      {/* cost analysis */}
+      <div className={styles.imageContainer}>
+        <h1 className={styles.h1}>
+          Optimize Kubernetes cluster deployment costs using monthly estimates
+          from <span>Kubecost.</span>
+        </h1>
+        <Image
+          className={styles.gif}
+          src='/demo-gifs/cost-analysis.gif'
+          alt='cost-analysis-gif'
+          width='850'
+          height='400'
+        />
+      </div>
+      {/* cluster visualization */}
+      <div className={styles.imageContainer}>
+        <h1 className={styles.h1}>
+          Visualize Kubernetes cluster and its resources, including pods,
+          services, and deployment, using <span>Kubeview.</span>
+        </h1>
+        <Image
+          className={styles.gif}
+          src='/demo-gifs/visualizer.gif'
+          alt='cluster-visualizer-gif'
+          width='850'
+          height='400'
+        />
+      </div>
     </div>
   );
 };
