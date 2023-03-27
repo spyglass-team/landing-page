759913eb47c16150f3f7eac6a90e5f0100dc5503
Cindy Chau <cindyxchau1@gmail.com>
2023-03-27 15:32:51 -0400

    added demo-gifs

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Animation from '../components/Animation';
+import Image from 'next/image';
 
 const Home = () => {
   return (
@@ -14,12 +15,28 @@ const Home = () => {
         <h1 className={styles.h1}>
           <span>EMPOWER YOUR KUBERNETES NEEDS WITH SPYGLASS!</span>
           <br />
-          <br />
           We are a one-stop shop for cluster health metrics, cluster
           visualization, and cost optimization.
         </h1>
         <Animation />
-        <h1></h1>
+        <Image
+          src='/demo-gifs/cluster-visualizer.gif'
+          alt='cluster-visualizer-gif'
+          width='400'
+          height='400'
+        />
+        <Image
+          src='/demo-gifs/local-cluster-metrics.gif'
+          alt='local-cluster-metrics-gif'
+          width='400'
+          height='400'
+        />
+        <Image
+          src='/demo-gifs/cost-analysis.gif'
+          alt='cost-analysis-gif'
+          width='400'
+          height='400'
+        />
       </div>
     </>
   );
