a8e87030b21132a76977a47c64696c822f8bf7a4
Cindy Chau <cindyxchau1@gmail.com>
2023-03-27 15:54:17 -0400

    added gifs to home page

---
+++ src/components/Features.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Image from 'next/image';
+import styles from '../styles/Features.module.css';
+
+const Features = () => {
+  return (
+    <div className={styles.container}>
+      <Image
+        src='/demo-gifs/cluster-visualizer.gif'
+        alt='cluster-visualizer-gif'
+        width='800'
+        height='400'
+      />
+      <Image
+        src='/demo-gifs/local-cluster-metrics.gif'
+        alt='local-cluster-metrics-gif'
+        width='800'
+        height='400'
+      />
+      <Image
+        src='/demo-gifs/cost-analysis.gif'
+        alt='cost-analysis-gif'
+        width='800'
+        height='400'
+      />
+    </div>
+  );
+};
+
+export default Features;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Animation from '../components/Animation';
-import Image from 'next/image';
+import Features from '../components/Features';
 
 const Home = () => {
   return (
@@ -15,29 +15,13 @@ const Home = () => {
         <h1 className={styles.h1}>
           <span>EMPOWER YOUR KUBERNETES NEEDS WITH SPYGLASS!</span>
           <br />
+          <br />
           We are a one-stop shop for cluster health metrics, cluster
           visualization, and cost optimization.
         </h1>
         <Animation />
-        <Image
-          src='/demo-gifs/cluster-visualizer.gif'
-          alt='cluster-visualizer-gif'
-          width='400'
-          height='400'
-        />
-        <Image
-          src='/demo-gifs/local-cluster-metrics.gif'
-          alt='local-cluster-metrics-gif'
-          width='400'
-          height='400'
-        />
-        <Image
-          src='/demo-gifs/cost-analysis.gif'
-          alt='cost-analysis-gif'
-          width='400'
-          height='400'
-        />
       </div>
+      <Features />
     </>
   );
 };
