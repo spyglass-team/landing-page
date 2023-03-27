084d794205e7f0b8eb05b6d9ca2a2f92fef7afed
Cindy Chau <cindyxchau1@gmail.com>
2023-03-27 19:34:36 -0400

    added github star butotn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
+      <Head>
+        <script async defer src='https://buttons.github.io/buttons.js'></script>
+      </Head>
       <Link href='/'>
         <Image
           src='/side-logo.png'
@@ -17,6 +21,16 @@ const Navbar = () => {
         <Link href='https://github.com/oslabs-beta/spyglass'>Github</Link>
         <Link href='#'>Press</Link>
         <Link href='/contact'>Contact Us</Link>
+        <Link
+          className='github-button'
+          href='https://github.com/oslabs-beta/spyglass'
+          data-icon='octicon-star'
+          data-size='large'
+          data-show-count='true'
+          aria-label='Star oslabs-beta/spyglass on GitHub'
+        >
+          Star
+        </Link>
       </div>
     </nav>
   );
