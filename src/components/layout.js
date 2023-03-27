2e16967fe05ed0aaf6572494fbb20586b47beedc
Cindy Chau <cindyxchau1@gmail.com>
2023-03-27 10:14:41 -0400

    modified letter casing

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import Navbar from './Navbar';
-import Footer from './footer';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from '@/components/layout';
+import Layout from '@/components/Layout';
 import '../styles/app.css';
 
 const app = ({ Component, pageProps }) => {
